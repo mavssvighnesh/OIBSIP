daf4ceb061e1bbbea5d3a1f2ef181b2bf8f939e8
mavssvighnesh <vighneshmandaleeka@gmail.com>
2023-11-19 20:45:34 +0530

    final code

--- TASK_4_BASIC_WHEATHER_APP/weatherapp.py
+++ TASK_4_BASIC_WHEATHER_APP/weatherapp.py
@@ -1,37 +1,53 @@
-import requests
+import requests  # importing the requests module
+import apikey  # this file contains the api key for this automation
 
-def fetch_weather_data(api_key, location):
-    base_url = "http://api.openweathermap.org/data/2.5/weather"
+
+# method to get the report from the weahter database
+def get_report(location):
+
+    # the main head url
+    main_url = "http://api.openweathermap.org/data/2.5/weather"
+
+    # copying the api key to the api_key variable
+    api_key = f"{apikey.api_key}"
+
+    # parameters list that should be appended to the url
     params = {
         "q": location,
         "appid": api_key,
-        "units": "metric"  # Use "imperial" for Fahrenheit
+        "units": "metric"
     }
+    response = requests.get(main_url, params)
 
-    response = requests.get(base_url, params=params)
-
-    if response.status_code==200:
-        weather_data=response.json()
-        return weather_data
+    # checking for the response message
+    if response.status_code == 200:
+        wheather_data = response.json()
+        return wheather_data
+    # condition to return the error message in case of error
     else:
         print("Failed to fetch weather data")
         return None
 
-def display_weather(weather_data):
+
+# method to display the obtained the weather data
+def display(weather_data):
     if weather_data:
+        # defining the format to print the data
         print(f"Weather in {weather_data['name']}:")
         print(f"Temperature: {weather_data['main']['temp']}°C")
         print(f"Humidity: {weather_data['main']['humidity']}%")
-        print(f"Weather Conditions: {weather_data['weather'][0]['description']}")
+        print(
+            f"Weather Conditions: {weather_data['weather'][0]['description']}")
+    # prints the message if the required location weather data is not available
     else:
         print("No weather data available.")
 
+# main method which encapsulates all the method callings
 def main():
-    api_key="3556c3f585905c4c1a9fb3cdded2070c" # Replace with your actual API key from OpenWeatherMap
-    location="Hyderbad"
+    print("WELCOME TO WHEATHER REPORTER")
+    location = input("ENTER A CITY NAME (eg.New York)  ")
+    data = get_report(location)
+    display(data)
 
-    weather_data=fetch_weather_data(api_key,location)
-
-    display_weather(weather_data)
 
 main()
